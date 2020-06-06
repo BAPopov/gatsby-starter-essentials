a2a7016075156b20f45b41a23df791585b4649a9
Borislav Popov <borislav.popov@clevertech.biz>
2020-06-06 19:02:32 +0300

    Add theme provider to index page and the button component for testing purposes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { ThemeProvider } from 'styled-components';
+import Theme from '../theme';
+
+import Button from '~components/Button';
 
 import Page from '~layout/Page';
 
 const IndexPage = () => (
-  <Page>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0 1.0875rem 1.45rem`
-      }}
-    >
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/404">Go to custom 404 page</Link>
-    </div>
-  </Page>
+  <ThemeProvider theme={Theme}>
+    <Page>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`
+        }}
+      >
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/404">Go to custom 404 page</Link>
+        <Button>Hello</Button>
+      </div>
+    </Page>
+  </ThemeProvider>
 );
 
 export default IndexPage;
